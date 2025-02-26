7e4de5f6854f779e1707a316a6b386d257a882b5
Timothy Jason <timothycaviezel@gmail.com>
2025-02-26 12:43:25 +0700

    FEAT: Add Warehouse management Product Settings

---
+++ warehouse_management_system/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Warehouse Management System",
+    'version': '17.0.1.0.0',
+    'category': 'Inventory',
+    'summary': """Detailed Warehouse""",
+    'description': "?",
+    'author': 'Timo',
+    'depends': ['base', 'product'],
+    'data': [
+        'views/product_template.xml'
+    ],
+    'assets': {
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}
---
+++ warehouse_management_system/models/product_template.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'product.template'
+
+    model = fields.Char(string="Model")
+    b_d_detail = fields.Char(string="B/D Detail")
+    b_d_date = fields.Date(string="B/D Date")
+    status = fields.Selection([
+        ('waiting_inspenction', 'Waiting Inspection'),
+        ('waiting_part', 'Waiting Part'),
+        ('unnder_progress', 'Under Progress'),
+    ], string="Status", tracking=True)
+    rfu_plan = fields.Char(string="Plan RFU")
+    part_description = fields.Text(string="Part Description")
+    no_mrd = fields.Char(string="No. MRD")
+    eta = fields.Char(string="ETA")
+    ata = fields.Char(string="ATA")
+
+
+    
